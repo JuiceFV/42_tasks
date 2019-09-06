e1735363937d6173a4ede395ce6f9d0a4d9bde67
Charmeleon Spider <cspider@at-o3.21-school.ru>
2019-09-06 16:38:08 +0300

    Get_Next_Line comments added

--- Get_Next_Line/get_next_line.c
+++ Get_Next_Line/get_next_line.c
@@ -6,26 +6,39 @@
 /*   By: cspider <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/06 14:12:55 by cspider           #+#    #+#             */
-/*   Updated: 2019/09/06 15:45:08 by cspider          ###   ########.fr       */
+/*   Updated: 2019/09/06 16:37:05 by cspider          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
+//append every (single) line into our "line"
 static	int	line_append(char **str, char **line)
 {
 	int		length;
 	char	*temp;
 
 	length = 0;
+	//count line's length
 	while ((*str)[length] != '\n' && (*str)[length] != '\0')
 		length++;
+	//if last symbol in the line is a "newline"
 	if ((*str)[length] == '\n')
 	{
+		//we save the string into line up to the length found
 		*line = ft_strsub(*str, 0, length);
+		//we readjust the stored data (**str)
+		//by creating a temp string that has
+		//the rest data after the "newline"
 		temp = ft_strdup(&((*str)[length + 1]));
+		//free the stored data to update it to
+		//the current address cause we already append
+		//a line from it
 		free(*str);
 		*str = temp;
+		//When we find the end of the file
+		//we free the memory cause it is not needed
+		//anymore
 		if ((*str)[0] == '\0')
 			ft_strdel(str);
 	}
@@ -47,9 +60,14 @@ static int	get_result(char **str, char **line, int result, int fd)
 		return (line_append(&str[fd], line));
 }
 
+//The function reads a file and returns the line
+//ending with a newline character from a file descriptor
 int			get_next_line(const int fd, char **line)
 {
 	int			result;
+	//a static variable so that whenever the function is called
+	//it (the variable) would store the result of the previous 
+	//function call
 	static char *str[FD_SIZE];
 	char		buff[BUFF_SIZE + 1];
 	char		*temp;
@@ -59,14 +77,28 @@ int			get_next_line(const int fd, char **line)
 	while ((result = read(fd, buff, BUFF_SIZE)) > 0)
 	{
 		buff[result] = '\0';
+		//When function is called at first time
+		//we check to see if our (*str) is empty.
+		//If it is we allocate memory for it using our buff string
 		if (str[fd] == NULL)
 			str[fd] = ft_strdup(buff);
 		else
 		{
+			//combine each string with other. 
+			//We using temp because we are only
+			//reading so many bytes at a time
+			//decided by our BUFF_SIZE. If we
+			//read at each iteration without
+			//memory freeing, then we would have
+			//memory leaks. So that we can keep
+			//track of how much is read and delete
+			//the previous stored data.
 			temp = ft_strjoin(str[fd], buff);
 			free(str[fd]);
 			str[fd] = temp;
 		}
+		//When a "newline" is encountered
+		//then loop breaks
 		if (ft_strchr(str[fd], '\n'))
 			break ;
 	}
