ead6cb6ec6f7bba16f62e5f0d19901bac652dc3b
JuiceFV <alexcar19622000@gmail.com>
2019-09-27 19:50:23 +0300

    added flags, precision, width, subspecifiers evaluate

--- Printf_2/sources/ft_option_handler.c
+++ Printf_2/sources/ft_option_handler.c
@@ -26,11 +26,67 @@ static void	evaluate_flags(t_printf_format* frmt)
 	}
 }
 
-void	evaluate_options(t_printf_format *frmt)
+static void evaluate_width_and_prec(t_printf_format* frmt)
+{
+	int str_width;
+
+	if (ft_isdigit(*frmt->format))
+	{
+		str_width = ft_atoi(frmt->format);
+		frmt->min_length = str_width & ((1 - str_width) >> 31) |
+			1 & (~(1 - str_width) >> 31);
+		while (ft_isdigit(*frmt->format))
+			frmt->format++;
+	}
+	if (*frmt->format == '.')
+	{
+		frmt->format++;
+		str_width = ft_atoi(frmt->format);
+		frmt->precision = str_width & (~(str_width) >> 31);
+		while (ft_isdigit(*frmt->format))
+			frmt->format++;
+		frmt->flags |= FLAGS_PRECISION;
+	}
+}
+
+static void	evaluate_subspecifiers(t_printf_format *frmt)
+{
+	while (1)
+	{ 
+		if (*frmt->format == 'l')
+			frmt->flags |= (frmt->format[1] == 'l' && ++frmt->format) ? FLAGS_LONG_LONG : FLAGS_LONG;
+		else if (*frmt->format == 'h')
+			frmt->flags |= (frmt->format[1] == 'h' && ++frmt->format) ? FLAGS_CHAR : FLAGS_SHORT;
+		else if (*frmt->format == 'j')
+			frmt->flags |= FLAGS_INTMAX;
+		else if (*frmt->format == 'z')
+			frmt->flags |= FLAGS_SIZE_T;
+		else
+			break ;
+		frmt->format++;
+	}
+}
+
+static void	evaluate_specifiers(t_printf_format* frmt)
+{
+	frmt->character = *frmt->format;
+	if (frmt->character == 's')
+		(frmt->flags & FLAGS_LONG || frmt->flags & FLAGS_LONG_LONG) ?
+		put_wchar_str(p) : put_char_str(p);
+}
+
+void	evaluate_format(t_printf_format *frmt)
 {
 	frmt->flags = 0U;
 	frmt->min_length = 0;
 	frmt->precision = 1U;
 	evaluate_flags(frmt);
+	evaluate_width_and_prec(frmt);
+	evaluate_subspecifiers(frmt);
+	evaluate_flags(frmt);
+	if (frmt->flags & FLAGS_PLUS)
+		frmt->flags &= ~FLAGS_SPACE;
+	if (ft_strchr("CDSUOBX", *frmt->format))
+		frmt->flags |= (*frmt->format != 'X') ? FLAGS_LONG : FLAGS_UPPERCASE;
 
 }
