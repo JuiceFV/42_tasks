a7f7cedf498c321b1d0d1aa67480c571108768ba
JuiceFV <alexcar19622000@gmail.com>
2019-09-27 19:52:13 +0300

    ft_printf.h modified

--- Printf_2/includes/ft_printf.h
+++ Printf_2/includes/ft_printf.h
@@ -18,7 +18,14 @@
 # define FLAGS_STAR			(1 << 5U)
 # define FLAGS_UPPERCASE	(1 << 6U)
 # define FLAGS_SHORT		(1 << 7U)
+# define FLAGS_CHAR			(1 << 8U)
+# define FLAGS_LONG			(1 << 9U)
+# define FLAGS_LONG_LONG	(1 << 10U)
+# define FLAGS_INTMAX		(1 << 11U)
+# define FLAGS_SIZE_T		(1 << 12U)
+# define FLAGS_MIN_LEN		(1 << 13U)
 # define FLAGS_PRECISION	(1 << 14U)
+# define FLAGS_POINTER		(1 << 15U)
 
 typedef	struct		s_printf_format
 {
@@ -31,12 +38,13 @@ typedef	struct		s_printf_format
 	unsigned int	flags;
 	size_t			min_length;
 	unsigned int	precision;
+	char			character;
 	int				temp;
 }					t_printf_format;
 
 int		ft_vsnprintf(t_printf_format frmt);
 
-void	evaluate_options(t_printf_format *frmt);
+void	evaluate_format(t_printf_format *frmt);
 
 int		get_char_index(char* s, int c);
 #endif
