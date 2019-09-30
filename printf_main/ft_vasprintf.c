6614764cfaa2e0abb9ea9521fe196ee8ff2c36fb
Charmeleon Spider <cspider@vo-j2.21-school.ru>
2019-09-30 15:28:47 +0300

    finished here. Added len_modifier and matcher

--- printf_main/ft_vasprintf.c
+++ printf_main/ft_vasprintf.c
@@ -6,13 +6,13 @@
 /*   By: cspider <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/30 12:51:42 by cspider           #+#    #+#             */
-/*   Updated: 2019/09/30 13:40:38 by cspider          ###   ########.fr       */
+/*   Updated: 2019/09/30 15:05:57 by cspider          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-static const char	*pf_evaluate_prec(const char *s,
+static const char	*evaluate_prec(const char *s,
                                     t_specifier *sp, va_list ap)
 {
     if (*s == '*')
@@ -26,7 +26,7 @@ static const char	*pf_evaluate_prec(const char *s,
     return (s);
 }
 
-static const char	*pf_evaluate_size(char const *s,
+static const char	*evaluate_size(char const *s,
 							t_specifier *sp, va_list ap)
 {
 	va_list	temp;
@@ -56,7 +56,7 @@ static const char	*pf_evaluate_size(char const *s,
 	return (s);
 }
 
-static	void		pf_set_len_modifier(char c, char *lm)
+static	void		set_len_modifier(char c, char *lm)
 {
 	if (c == 'h' && *lem == 'h')
 		*lm = ft_toupper(c);
@@ -66,13 +66,13 @@ static	void		pf_set_len_modifier(char c, char *lm)
 		*lm = c;
 }
 
-static const char	pf_match(char const *s, t_specifier *sp, va_list ap)
+static const char	match(char const *s, t_specifier *sp, va_list ap)
 {
 	*sp = NEW_SPECIFIER;
 	while (*(++s))
 	{
 		if (*s == '.')
-			s = pf_evaluate_prec(s + 1, sp, va) - 1;
+			s = evaluate_prec(s + 1, sp, va) - 1;
 		else if (*s == '\'')
 			sp->qoute = 1;
 		else if (*s == '$')
@@ -81,8 +81,14 @@ static const char	pf_match(char const *s, t_specifier *sp, va_list ap)
 			sp->size = 0;
 		}
 		else if (ft_isdigit(*s) || *s == '*')
-			s = pf_evaluate_size(s, sp, va) - 1;
-		else if
+			s = evaluate_size(s, sp, va) - 1;
+		else if ((char_index(*s, "0+- #_")) >= 0)
+			sp->flags[char_index(*s, "0+- #_")] = 1;
+		else if ((char_index(*s, "hljztL")) >= 0)
+			set_len_modifier(*s, &(sp->len_mod));
+		else if ((sp->conv = *s))
+			return (s + 1);
 	}
+	return (s);
 }
 
