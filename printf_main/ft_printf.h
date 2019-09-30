40caa120124a729124f347554b906fc0035c1b12
Charmeleon Spider <cspider@vo-j2.21-school.ru>
2019-09-30 15:26:08 +0300

    added vector and needed in modification

--- printf_main/ft_printf.h
+++ printf_main/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: cspider <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/30 12:50:39 by cspider           #+#    #+#             */
-/*   Updated: 2019/09/30 13:40:51 by cspider          ###   ########.fr       */
+/*   Updated: 2019/09/30 15:23:27 by cspider          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,13 +17,46 @@
 # include <stdarg.h>
 
 #define NEW_SPECIFIER (t_specifier){{{0, 0, 0, 0, 0, 0}}, 0, 0, 0, 0, 0, 0, -1}
+/*
+** Simple vector
+*/
+typedef struct	s_vector
+{
+	void*		data;
+	size_t		size;
+	size_t		type_size;
+}				t_vector;
+/*
+** Constructor
+*/
+# define VECTOR(T) (t_vector){NULL, 0, 0, sizeof(T)}
+
+t_vector		vector_new(size_t type_size);
+t_vector		vector_alloc(size_t type_size);
+int				vector_append(t_vector *this, void const *data, size_t datalen);
+
 
 typedef struct  s_specifiers
 {
-	int			dollar_size;
-	int			quote;
-	int			size;
-    int         precision;
+	union
+	{
+		char	f[6];
+		struct
+		{
+			char	zero;
+			char	plus;
+			char	minus;
+			char	space;
+			char	sharp;
+			char	underscore;	
+		}			n;
+	}				flags;
+	char			conv;
+	char			len_mod;
+	int				dollar_size;
+	int				quote;
+	int				size;
+    int         	precision;
 }               t_specifiers;
 /*
 ** main printf functions
@@ -43,5 +76,9 @@ int             ft_printf(const char *format, ...);
 */
 int             ft_vprintf(char const *format, va_list ap);
 int             ft_vdprintf(int fd, char const *format, va_list ap);
+/*
+** Helpful Utilits
+*/
+int				char_index(char i, char const *str);
 
 #endif
