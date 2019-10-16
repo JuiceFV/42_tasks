a19996c144a12267fe89fa20afdf5fd8b7d9bdee
JuiceFV <alexcar19622000@gmail.com>
2019-10-16 17:21:34 +0300

    header has been updated

--- push_swap/push_swap/push_swap.h
+++ push_swap/push_swap/push_swap.h
@@ -14,10 +14,23 @@ typedef struct 			s_ps_stack
 {
 	t_ps_node			*begin;
 	t_ps_node			*end;
-	t_ps_node			**p;
+	t_ps_node			**els;
 	int					top;
 	int					argc;
 }						t_ps_stack;
+
+/*
+** stack operations:
+*/
+
+int	create_stack(t_ps_stack *a, t_ps_stack *b, char **argv, int argc);
+int stck_append_end(char *str, t_ps_node **back);
+
+/*
+**	error detection functions:
+**	<is_int> - checks an each argument compliance with the int.
+**	<is_dupl> - looking for duplicates 
+*/
 
 int	is_int(int argc, char **argv);
 int is_dupl(t_ps_stack *a, int argc);
